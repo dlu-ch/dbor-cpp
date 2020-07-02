eddf40f1e729acd2a9111e8dbdda23e586eb2706
Daniel Lutz <dlu-ch@users.noreply.github.com>
2020-07-02 15:24:04 +0200

    Make a dlb project

---
+++ build.py
@@ -0,0 +1,64 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# dbor-c++ - C++ implementation of DBOR encoder and decoder
+# Copyright (C) 2020 Daniel Lutz <dlu-ch@users.noreply.github.com>
+
+# Run this script by one of the following shell commands:
+#
+#    dlb build-all                  # from anywhere in the working tree (with directory of 'dlb' in $PATH)
+#    python3 -m build-all           # in the directory of this file
+#    python3 "$PWD"/build-all.py'   # in the directory of this file
+
+import sys
+
+import dlb.di
+import dlb.fs
+import dlb.ex
+import dlb_contrib.gcc
+import dlb_contrib.iso6429
+
+if sys.stderr.isatty():
+    # assume terminal compliant with ISO/IEC 6429 ("VT-100 compatible")
+    dlb.di.set_output_file(dlb_contrib.iso6429.MessageColorator(sys.stderr))
+
+
+class Path(dlb.fs.PosixPath, dlb.fs.WindowsPath, dlb.fs.NoSpacePath):
+    pass
+
+
+class CCompiler(dlb_contrib.gcc.CCompilerGcc):
+    DIALECT = 'c11'
+
+
+class CLinker(dlb_contrib.gcc.CLinkerGcc):
+    pass
+
+
+with dlb.ex.Context():
+    source_directory = Path('src/')
+    test_directory = Path('test/')
+    output_directory = Path('build/out/')
+
+    with dlb.di.Cluster('compile'), dlb.ex.Context():
+        compile_results = [
+            CCompiler(
+                source_files=[p],
+                object_files=[output_directory / p.with_appended_suffix('.o')],
+                include_search_directories=[source_directory]
+            ).start()
+            for p in source_directory.iterdir(name_filter=r'.+\.cpp', is_dir=False)
+        ] + [
+            CCompiler(
+                source_files=[p],
+                object_files=[output_directory / p.with_appended_suffix('.o')],
+                include_search_directories=[source_directory]
+            ).start()
+            for p in test_directory.iterdir(name_filter=r'.+\.cpp', is_dir=False)
+        ]
+            
+
+    with dlb.di.Cluster('link'), dlb.ex.Context():        
+        application_file = CLinker(
+            object_and_archive_files=[r.object_files[0] for r in compile_results],
+            linked_file=output_directory / 'tester').start().linked_file
+
+dlb.di.inform(f'test application size: {application_file.native.raw.stat().st_size} B')
---
+++ test/main.cpp
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+// dbor-c++ - C++ implementation of DBOR encoder and decoder
+// Copyright (C) 2020 Daniel Lutz <dlu-ch@users.noreply.github.com>
+
+int main() {
+    return 0;
+}
